48b5561f70aedbb7a42dfeadff5258077862929f
Matthew Murray <mattm898865@gmail.com>
2022-03-13 23:25:49 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const Post_1 = require("./entities/Post");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [Post_1.Post],
+        dbName: 'redditClone',
+        debug: !constants_1.__prod__,
+        type: 'postgresql'
+    });
+    const post = orm.em.create(Post_1.Post, { title: 'my first post' });
+};
+main();
+//# sourceMappingURL=index.js.map
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,11 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from '@mikro-orm/core';
+
+export default {
+    entities: [Post],
+    dbName: 'redditClone',
+    type: 'postgresql',
+    debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
+
