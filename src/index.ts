6d460fbe9452e2b9b3bca143bbbbae8239f70f0b
Matthew Murray <mattm898865@gmail.com>
2022-03-14 13:28:26 -0500

    graphql added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
 import { MikroORM } from "@mikro-orm/core"
+import 'reflect-metadata'
 import { __prod__ } from './constants';
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
+import { ApolloServer } from'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
 
-console.log('dirname', __dirname)
-    // const orm = await MikroORM.init(microConfig)
+    const orm = await MikroORM.init(microConfig)
+    await orm.getMigrator().up()
 
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post)
-    // console.log('------sql 2-------')
-    // await orm.em.nativeInsert(Post, {title: "my first post"})
+    const app = express()
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    })
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
 }
+
 
 main()
