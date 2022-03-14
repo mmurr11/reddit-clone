b8552fad16081dc6dcc2c50f8bc36934bfd4fd64
Matthew Murray <mattm898865@gmail.com>
2022-03-14 11:58:51 -0500

    migrations

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,13 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
 
-    const orm = await MikroORM.init(microConfig)
+console.log('dirname', __dirname)
+    // const orm = await MikroORM.init(microConfig)
 
-    const post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(post)
-    console.log('------sql 2-------')
-    await orm.em.nativeInsert(Post, {title: "my first post"})
+    // const post = orm.em.create(Post, {title: 'my first post'});
+    // await orm.em.persistAndFlush(post)
+    // console.log('------sql 2-------')
+    // await orm.em.nativeInsert(Post, {title: "my first post"})
 }
 
 main()
