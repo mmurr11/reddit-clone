cdcbfc291349ca4e89fd908085f20ff146a86389
Matthew Murray <mattm898865@gmail.com>
2022-03-14 19:55:52 -0500

    CRUD and Auth

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
+require("reflect-metadata");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const express_1 = __importDefault(require("express"));
 const apollo_server_express_1 = require("apollo-server-express");
