b3fe6a131e145737f271e5ae2a5a66dab3d9e024
Matthew Murray <mattm898865@gmail.com>
2022-03-21 22:35:27 -0500

    isServer boolean util. Navbar doesnt make query

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import { Box, Link, Flex, Button } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useMeQuery, useLogoutMutation } from "../generated/graphql";
+import { isServer } from "../../utils/isServer";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
   const [{ fetching: logoutFethcing }, logout] = useLogoutMutation();
   let body = null;
 
