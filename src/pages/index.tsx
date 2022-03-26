5550b45fcccad574978465fd3ba67236423b8bfa
Matthew Murray <mattm898865@gmail.com>
2022-03-26 11:36:19 -0500

    title and text within box and sliced

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { createUrqlClient } from "../../utils/createUrqlClient";
 import { withUrqlClient } from "next-urql";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
-import { Link } from "@chakra-ui/react";
+import { Text, Link, Stack, Box, Heading } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 const Index = () => {
@@ -20,7 +20,14 @@ const Index = () => {
       {!data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.text.slice(0, 50) + "..."}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   );
